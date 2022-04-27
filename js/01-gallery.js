721e213435ba40d49900949193034c5ffdaafd8f
Hanna-Pylypenko <pilipenkog679@gmail.com>
2022-04-27 23:27:54 +0300

    Rendered gallery items collection

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import {galleryItems} from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+createGalleryItemsMarkup(galleryItems)
+function createGalleryItemsMarkup(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `<div class="gallery__item">
+                 <a class="gallery__link" href = "${original}">
+                   <img
+                  class="gallery__image"
+                  src="${preview}"
+                   data-source="${original}"
+                   alt="${description}"/>
+                 </a>
+               </div> `}).join(' ')
+
+}
+
+galleryEl.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+console.log(galleryEl);
