eccbd68b5dbdcfe1c4b8381ccb1dc729b14e40cb
Hanna-Pylypenko <pilipenkog679@gmail.com>
2022-04-29 18:35:39 +0300

    finished with the second task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,18 +1,17 @@
-import {galleryItems} from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const galleryEl = document.querySelector('.gallery');
+const galleryEl = document.querySelector(".gallery");
 
-galleryEl.addEventListener('click', onGalleryClick);
-
+galleryEl.addEventListener("click", onGalleryClick);
 
 const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
-galleryEl.insertAdjacentHTML('beforeend', galleryItemsMarkup);
-
+galleryEl.insertAdjacentHTML("beforeend", galleryItemsMarkup);
 
 function createGalleryItemsMarkup(galleryItems) {
-    return galleryItems.map(({ preview, original, description }) => {
-        return `<div class="gallery__item">
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
                  <a class="gallery__link" href = "${original}">
                    <img
                   class="gallery__image"
@@ -20,16 +19,24 @@ function createGalleryItemsMarkup(galleryItems) {
                    data-source="${original}"
                    alt="${description}"/>
                  </a>
-               </div> `}).join(' ');
-
+               </div> `;
+    })
+    .join(" ");
 }
-
 
 console.log(galleryEl);
 
 function onGalleryClick(event) {
-    event.preventDefault();
-    console.log('clicky-click');
-    ;
-}
+  event.preventDefault();
+  const largePicUrl = event.target.dataset.source;
+  const instance = basicLightbox.create(` <img src="${largePicUrl}">`);
 
+  instance.show();
+
+  // galleryEl.addEventListener("keydown", (event) => {
+  //   if (event.code === 27) {
+  //     // close modal here
+  //     instance.element().querySelector("img").onkeydown = instance.close;
+  //   }
+  // });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
+console.log(galleryEl);
 
-console.log(galleryItems);
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" /></a> `;
+    })
+    .join(" ");
+}
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
