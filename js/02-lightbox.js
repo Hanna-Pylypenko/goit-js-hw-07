9c0ef618cb116052b667dd7f452d1c36998cf305
Hanna-Pylypenko <pilipenkog679@gmail.com>
2022-05-01 23:23:16 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -9,8 +9,8 @@ galleryEl.insertAdjacentHTML("beforeend", galleryItemsMarkup);
 function createGalleryItemsMarkup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
-      return `<a class="gallery__item" href="${original}">
-  <img class="gallery__image" src="${preview}" alt="${description}" /></a> `;
+      return `<div class="gallery__item"> <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" /></a> </div>`;
     })
     .join(" ");
 }
